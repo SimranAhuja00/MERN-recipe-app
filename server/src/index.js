f5cd7e4cc9d1c76668ac563f3bd98989c1eb1510
SimranAhuja00 <simranahuja0308@gmail.com>
2023-08-06 14:50:33 +0530

    add

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://2021simranahuja:hemenandtanay2003@recipes.6li1zwx.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log("Server started"));
